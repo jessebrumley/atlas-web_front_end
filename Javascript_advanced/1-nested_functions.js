36e19f559017d39c0cc1021d1dc4d341c98d93b9
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-07-10 21:26:19 -0500

    1

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,24 @@
+let globalVariable = "Welcome";
+
+function outer() {
+  alert(globalVariable);
+  let course = "Holberton";
+
+  function inner() {
+    alert(globalVariable);
+    alert(course);
+    let exclamation = "!";
+
+    function inception() {
+      alert(globalVariable);
+      alert(course);
+      alert(exclamation);
+    }
+
+    inception();
+  }
+
+  inner();
+}
+
+outer();
