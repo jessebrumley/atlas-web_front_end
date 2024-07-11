37cf4c2ccacd27d743c13f7cec7b5db2873b13af
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-07-11 11:40:54 -0500

    Using promises to force the calculation to be at the end of the stack but it actually make it take 25% longer to execute

--- Javascript_advanced/11-prime.js
+++ Javascript_advanced/11-prime.js
@@ -1,28 +1,37 @@
-function countPrimeNumbers(max) {
-  function isPrime(number) {
-    if (number <= 1) return false;
-    if (number === 2) return true;
-    if (number % 2 === 0) return false;
-    for (let i = 3; i * i <= number; i++) {
-      if (number % i === 0) return false;
-    }
-    return true;
-  }
+async function main() {
+  const promises = Array.from({ length: 100 }, () => countPrimeNumbers(100));
 
-  let count = 0;
-
-  for (let i = 1; i < max; i++) {
-    if (isPrime(i)) {
-      count++;
-    }
-  }
-
-  return count;
+  await Promise.all(promises);
 }
-const start = performance.now();
-for (let i = 0; i < 100; i++) {
-  countPrimeNumbers(100);
-  }
-const end = performance.now();  
 
-console.log(`Execution time of printing countPrimeNumbers was ${end - start} milliseconds.`);
+function countPrimeNumbers(max) {
+  return new Promise((resolve) => {
+    let count = 0;
+
+    for (let i = 1; i < max; i++) {
+      if (isPrime(i)) {
+        count++;
+      }
+    }
+
+    resolve(count);
+  });
+}
+
+function isPrime(number) {
+  if (number <= 1) return false;
+  if (number === 2) return true;
+  if (number % 2 === 0) return false;
+  for (let i = 3; i * i <= number; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+const start = performance.now();
+
+main().catch(console.error);
+
+const end = performance.now();
+
+console.log(`Execution time of the entire operation was ${end - start} milliseconds.`);
