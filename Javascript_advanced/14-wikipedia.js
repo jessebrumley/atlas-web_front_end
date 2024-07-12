7cde60c4a19d81eac4ce8109f21eff9d7c2bd2dc
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-07-11 21:48:21 -0500

    I assume this work because wikipedias content security policy is blocking the request but idk for sure

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -1,9 +1,30 @@
 function createElement(data) {
-  const paragraph = document.createElement('p');
-  paragraph.textContent = data;
-  document.body.appendChild(paragraph);
+  const para = document.createElement('p');
+  
+  para.textContent = data;
+  
+  document.body.appendChild(para);
 }
 
 function queryWikipedia(callback) {
-  XMLHttpRequest()
+  var xhr = new XMLHttpRequest();
+  
+  var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
+  
+  xhr.open('GET', url, true);
+  
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          
+          var extract = response.query.pages[0].extract;
+          
+          callback(extract);
+      }
+  };
+  
+  xhr.send();
 }
+
+queryWikipedia(createElement);
+
